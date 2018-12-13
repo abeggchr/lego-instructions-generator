b055567ce84b10cdab80ab8b54f71ddcdc881772
Christian Abegg <christian@abegg.rocks>
2018-12-13 21:47:49 +0100

    Add project files.

---
+++ LegoInstructionGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LegoInstructionGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
