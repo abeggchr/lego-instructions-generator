4aab7cec65add5fed9b1ade49db6587cecacc90f
Christian Abegg <christian@abegg.rocks>
2018-12-13 22:25:36 +0100

    args for input and output folder

--- LegoInstructionGenerator/Program.cs
+++ LegoInstructionGenerator/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using CommandLine;
+using System.IO;
 
 namespace LegoInstructionGenerator
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Parser.Default.ParseArguments<Options>(args).WithParsed(o =>
+            {
+                Console.WriteLine($"Generating from {o.InputPath}");
+                if (string.IsNullOrEmpty(o.OutputPath))
+                {
+                    o.OutputPath = Path.Combine(Path.GetTempPath(), new Guid().ToString());
+                    Directory.CreateDirectory(o.OutputPath);
+                }
+                Console.WriteLine($"Generating to {o.OutputPath}");
+            });
+
+            Console.Read();
+        }
+
+        public class Options
+        {
+            [Option('i', "input", Required = true, HelpText = "Input path containing 'generate.txt' file an images.")]
+            public string InputPath { get; set; }
+
+            [Option('o', "output", Required = false, HelpText = "Output path where the instruction is generated to.")]
+            public string OutputPath { get; set; }
         }
     }
 }
